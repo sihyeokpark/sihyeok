dad640e1477b8b64435dec6a72e1d71c4b1fb7d4
시혁 박 <exon.indiv@gmail.com>
2021-12-29 21:49:45 +0900

    Add: Home header

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import '../css/App.css'
+import '../css/Home.css'
 
 import Bar from '../components/Bar'
+
 
 export default function Home() {
     return (
         <div>
             <Bar />
+            <div className="header">
+                <h1>SIHYEOK</h1>
+                <p>Made by exon</p>
+            </div>
         </div>
     )
 }
