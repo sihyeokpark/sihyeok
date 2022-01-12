bef5dae9837a5914ce9c4df2271fcde5bd5fc28d
시혁 박 <exon.indiv@gmail.com>
2022-01-12 22:02:23 +0900

    Edit: Home

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -5,8 +5,10 @@ import Bar from '../components/Bar'
 
 export default function Home() {
     return (
-        <header>
-            <Bar />
+        <div className="root">
+            <header>
+                <Bar />
+            </header>
             <div className="header">
                 <h1>SIHYEOK</h1>
                 <p>Made by <a href="https://github.com/1-EXON">exon</a></p>
@@ -17,6 +19,17 @@ export default function Home() {
                 <p><a href="">SIHYEOK</a> 은 창의력 문제를 풀고 만들 수 있게 만들어진 프로젝트입니다.</p>
                 <button>지금 시작하기</button>
             </div>
-        </header>
+            <div className="ranking">
+                <h1>인기 문제</h1>
+                <div className="box">
+                    <h1>버스 문제</h1>
+                </div>
+            </div>
+            <footer>
+                <div className="footer">
+                    <p>푸터푸터푸터</p>
+                </div>
+            </footer>
+        </div>
     )
 }
