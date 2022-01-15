6d0816e1b49204269a8b031393ece081f44ac16a
시혁 박 <exon.indiv@gmail.com>
2022-01-15 12:35:02 +0900

    Edit: footer, scrollbar

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -27,7 +27,10 @@ export default function Home() {
             </div>
             <footer>
                 <div className="footer">
+                    <p>made by <a href="https://github.com/1-EXON">exon</a></p>
+                    <br></br>
                     <p>powered by <a href='https://exon.kr'>exon.kr</a></p>
+                    <p>© {(new Date()).getFullYear()} All Rights Reserved.</p>
                 </div>
             </footer>
         </div>
