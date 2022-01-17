14439e18c730b4522cdbd80dee8d84a27e8b02c7
시혁 박 <exon.indiv@gmail.com>
2022-01-17 20:07:11 +0900

    Add: Problems && Edit: CSS var

--- app/src/pages/Problems.tsx
+++ app/src/pages/Problems.tsx
@@ -1,3 +1,5 @@
+import '../css/Problems.css'
+
 import Bar from '../components/Bar'
 
 export default function Problems() {
@@ -6,7 +8,53 @@ export default function Problems() {
             <header>
                 <Bar />
             </header>
-            
+            <div className="nav">
+                <div className="left">
+                    <button>글쓰기</button>
+                </div>
+                <div className="right">
+                    <button>문제 만들기</button>
+                </div>
+            </div>
+            <div className='table'>
+                <table>
+                    <tr>
+                        <th>난이도</th>
+                        <th>문제</th>
+                        <th>정보</th>
+                    </tr>
+                    <tr>
+                        <td>골드1</td>
+                        <td>주차장</td>
+                        <td>사칙연산</td>
+                    </tr>
+                    <tr>
+                        <td>실버3</td>
+                        <td>9999999</td>
+                        <td>한자</td>
+                    </tr>
+                    <tr>
+                        <td>골드1</td>
+                        <td>주차장</td>
+                        <td>사칙연산</td>
+                    </tr>
+                    <tr>
+                        <td>실버3</td>
+                        <td>9999999</td>
+                        <td>한자</td>
+                    </tr>
+                    <tr>
+                        <td>골드1</td>
+                        <td>주차장</td>
+                        <td>사칙연산</td>
+                    </tr>
+                    <tr>
+                        <td>실버3</td>
+                        <td>9999999</td>
+                        <td>한자</td>
+                    </tr>
+                </table>
+            </div>
             
         </div>
     )
