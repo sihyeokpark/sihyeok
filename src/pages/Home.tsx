aad5d74a43c08c723b72628f0da81a11c9d7d2e6
EXON <exon.indiv@gmail.com>
2022-07-29 16:55:01 +0000

    Update: name, design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,15 +14,18 @@ export default function Home() {
                 <p>Made by <a href="https://github.com/1-EXON">exon</a></p>
             </div>
             <div className="main">
-                <h1><b>모두</b>가 문제를 내고</h1>
-                <h1>푸는 <b>창의력 문제 사이트</b></h1>
+                <h1><span className="highlight"><b>모두</b>가 문제를 내고</span></h1>
+                <h1><span className="highlight">푸는 <b className="gradient">창의력 문제 사이트</b></span></h1>
                 <p><a href="/">SIHYEOK</a> 은 창의력 문제를 풀고 만들 수 있게 만들어진 프로젝트입니다.</p>
                 <button>지금 시작하기</button>
             </div>
             <div className="ranking">
                 <h1>인기 문제</h1>
                 <div className="box">
-                    <h1>버스 문제</h1>
+                    <h1>박시혁의 문제</h1>
+                    <h1>박시혁의 문제</h1>
+                    <h1>박시혁의 문제</h1>
+                    <h1>박시혁의 문제</h1>
                 </div>
             </div>
             <Footer />
